c5da2bd329aef22043cbab9e9057d63738864e31
Mr. Doge <78793304+mrdoge0@users.noreply.github.com>
2025-11-25 12:28:01 +0300

    Update PREP-ME-TO-ZIP.sh

--- PREP-ME-TO-ZIP.sh
+++ PREP-ME-TO-ZIP.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
-rm -rf .git
+rm -rf .git || exit 1
 ORIGPWD="${PWD}"
-cd system/priv-app/NTCamera
-unzip *.zip
-rm *.zip
-cd "${ORIGPWD}"
+cd system/priv-app/NTCamera || exit 1
+unzip *.zip || exit 1
+rm *.zip || exit 1
+cd "${ORIGPWD}" || exit 1
+rm PREP-ME-TO-ZIP.sh || exit 1
+echo "source is prepped to package"
+exit 0
